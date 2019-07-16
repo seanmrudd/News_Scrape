50a8f0ec05ada7e2e3509af127eb6757651d3af2
shrudder <seanmrudd@me.com>
2019-07-16 01:39:09 -0400

    able to scrape data and console to cli

--- server.js
+++ server.js
@@ -2,35 +2,29 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
 var db = require("./models");
 
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var exphbs = require("express-handlebars");
 
-// Start the server
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/newsController");
+
+app.use(routes);
+
+mongoose.connect("mongodb://localhost/news_scrape", { useNewUrlParser: true });
+
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
-  });
+});
   
